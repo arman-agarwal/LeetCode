d7cf962b0da4fad48e491397c43e55cbe8223eda
Arman Agarwal <armanagarwal0308@gmail.com>
2022-11-17 23:21:09 -0500

    Time: 2 ms (24.99%), Space: 43.3 MB (11.63%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,32 @@
+//transpose and reverse left to right
+class Solution {
+    public void rotate(int[][] matrix) {
+        long startTime = System.nanoTime();
+        int n = matrix.length;
+        transpose(matrix, n);
+        reverse(matrix, n);
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println(totalTime);
+    }
+    
+    private void transpose(int[][] matrix,int n){
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    
+    private void reverse(int[][] matrix, int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<(n/2); j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+    }
+}
